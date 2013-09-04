29045cfd3a1a95151b8121d15ae396c0d6596e3f
Daniel Holbach <daniel.holbach@canonical.com>
2013-09-04 10:25:42 +0200

    only add desktop files if they are actually present, simplifies the rest of the code somewhat

--- cr_desktop.py
+++ cr_desktop.py
@@ -32,7 +32,9 @@ class ClickReviewDesktop(ClickReview):
             if not isinstance(self.manifest['hooks'][app]['desktop'], str):
                 error("manifest malformed: hooks/%s/desktop is not str" % app)
             d = self.manifest['hooks'][app]['desktop']
-            self.desktop_files[app] = os.path.join(self.unpack_dir, d)
+            full_fn = os.path.join(self.unpack_dir, d)
+            if os.path.exists(full_fn):
+                self.desktop_files[app] = full_fn
 
     def check_desktop_file(self):
         '''Check desktop file'''
@@ -45,35 +47,33 @@ class ClickReviewDesktop(ClickReview):
         self._add_result(t, n, s)
         for app in sorted(self.desktop_files):
             d = self.manifest['hooks'][app]['desktop']
-            full_fn = os.path.join(self.unpack_dir, d)
-            if os.path.exists(full_fn):         # should always be in top level, right?
-                content = open_file_read(full_fn).readlines()
+            content = open_file_read(self.desktop_files[app]).readlines()
 
-                # desktop_icon tests
-                icon_lines = list(filter(lambda l: l.startswith('Icon='), content))
-                t = 'info'
-                n = 'icon_specified'
-                s = 'OK (%s)' % d
-                if not icon_lines:
-                    t = 'warn'
-                    s = 'No icon specified in .desktop file `%s`.' % d
-                self._add_result(t, n, s)
+            # desktop_icon tests
+            icon_lines = list(filter(lambda l: l.startswith('Icon='), content))
+            t = 'info'
+            n = 'icon_specified (%s)' % d
+            s = 'OK'
+            if not icon_lines:
+                t = 'warn'
+                s = 'No icon specified in .desktop file.'
+            self._add_result(t, n, s)
 
-                t = 'info'
-                n = 'one_icon_specified'
-                s = 'OK (%s)' % d
-                if len(icon_lines) > 1:
-                    t = 'warn'
-                    s = 'More than one icon line specified in .desktop file `%s`.' % d
-                self._add_result(t, n, s)
-                icon_path = icon_lines[0].split("=")[1].strip()
-                
-                # https://public.apps.ubuntu.com/download/com.ubuntu.developer.mhall119/uReadIt/com.ubuntu.developer.mhall119.uReadIt-0.9.1.click?noauth=1
-                t = 'info'
-                n = 'icon_full_path'
-                s = 'OK (%s)' % d
-                if icon_path.startswith('/'):
-                    t = 'error'
-                    s = 'Absolute path `%s` for icon given in .desktop file `%s`' % \
-                            (icon_path, d)
-                self._add_result(t, n, s)
+            t = 'info'
+            n = 'one_icon_specified (%s)' % d
+            s = 'OK'
+            if len(icon_lines) > 1:
+                t = 'warn'
+                s = 'More than one icon line specified in .desktop file.'
+            self._add_result(t, n, s)
+            icon_path = icon_lines[0].split("=")[1].strip()
+            
+            # https://public.apps.ubuntu.com/download/com.ubuntu.developer.mhall119/uReadIt/com.ubuntu.developer.mhall119.uReadIt-0.9.1.click?noauth=1
+            t = 'info'
+            n = 'icon_full_path (%s)' % d
+            s = 'OK'
+            if icon_path.startswith('/'):
+                t = 'error'
+                s = 'Absolute path `%s` for icon given in .desktop file.' \
+                        % icon_path
+            self._add_result(t, n, s)
