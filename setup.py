ee200dd97c4373bb93c35d2ccab869458cb672ea
Daniel Holbach <daniel.holbach@canonical.com>
2013-09-26 12:42:02 +0200

    add build system

---
+++ setup.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+
+from setuptools import setup, find_packages
+import glob
+import os
+import re
+
+# look/set what version we have
+changelog = 'debian/changelog'
+if os.path.exists(changelog):
+    head = open(changelog).readline()
+    match = re.compile('.*\((.*)\).*').match(head)
+    if match:
+        version = match.group(1)
+
+scripts = glob.glob('bin/*')
+setup(name='click-reviewers-tools',
+      version=version,
+      scripts=scripts,
+      packages=find_packages(),
+      test_suite='clickreviews.tests'
+)
