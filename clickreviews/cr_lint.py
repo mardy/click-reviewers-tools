46e04d80ae6866895de9928cc096664d892dc8ce
Jamie Strandboge <jamie@canonical.com>
2014-01-06 13:47:00 -0600

    update copyright dates for changed files in the last commit

--- clickreviews/cr_lint.py
+++ clickreviews/cr_lint.py
@@ -1,6 +1,6 @@
 '''cr_lint.py: click lint checks'''
 #
-# Copyright (C) 2013 Canonical Ltd.
+# Copyright (C) 2013-2014 Canonical Ltd.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
--- clickreviews/tests/test_cr_lint.py
+++ clickreviews/tests/test_cr_lint.py
@@ -1,6 +1,6 @@
 '''test_cr_lint.py: tests for the cr_lint module'''
 #
-# Copyright (C) 2013 Canonical Ltd.
+# Copyright (C) 2013-2014 Canonical Ltd.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
