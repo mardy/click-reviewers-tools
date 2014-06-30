d55f79004f0b78b4501b9b5ba139e56b343c59e7
Daniel Holbach <daniel.holbach@canonical.com>
2014-06-30 15:08:33 +0200

    make use of new data file

--- clickreviews/frameworks.py
+++ clickreviews/frameworks.py
@@ -3,26 +3,13 @@ This file defines all known frameworks and their current status.
 Frameworks are currenly tracked in: http://goo.gl/z9ohJ3
 """
 
-FRAMEWORKS = {
-    'ubuntu-sdk-13.10': 'deprecated',
-    'ubuntu-sdk-14.04-dev1': 'deprecated',
-    'ubuntu-sdk-14.04-html-dev1': 'deprecated',
-    'ubuntu-sdk-14.04-papi-dev1': 'deprecated',
-    'ubuntu-sdk-14.04-qml-dev1': 'deprecated',
-    'ubuntu-sdk-14.04': 'available',
-    'ubuntu-sdk-14.04-html': 'available',
-    'ubuntu-sdk-14.04-papi': 'available',
-    'ubuntu-sdk-14.04-qml': 'available',
-    'ubuntu-sdk-14.10-dev1': 'obsolete',
-    'ubuntu-sdk-14.10-html-dev1': 'obsolete',
-    'ubuntu-sdk-14.10-papi-dev1': 'obsolete',
-    'ubuntu-sdk-14.10-qml-dev1': 'obsolete',
-    'ubuntu-sdk-14.10-dev2': 'available',
-    'ubuntu-sdk-14.10-html-dev2': 'available',
-    'ubuntu-sdk-14.10-papi-dev2': 'available',
-    'ubuntu-sdk-14.10-qml-dev2': 'available',
-}
+import json
+import os
 
+DATA_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), 
+                                        '../data/'))
+FRAMEWORKS_FILE = os.path.join(DATA_DIR, 'frameworks.json')
+FRAMEWORKS = json.loads(open(FRAMEWORKS_FILE, 'r').read())
 
 DEPRECATED_FRAMEWORKS = []
 OBSOLETE_FRAMEWORKS = []
