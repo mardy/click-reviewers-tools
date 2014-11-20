65cb0e7567f0540b4bada14f172c779c1d7ac22c
Jamie Strandboge <jamie@ubuntu.com>
2014-11-20 09:23:28 -0600

    add bin-path click hook checks and tests

---
+++ clickreviews/cr_bin_path.py
@@ -0,0 +1,60 @@
+'''cr_bin_path.py: click bin-path'''
+#
+# Copyright (C) 2014 Canonical Ltd.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import print_function
+
+from clickreviews.cr_common import ClickReview
+import os
+
+
+class ClickReviewBinPath(ClickReview):
+    '''This class represents click lint reviews'''
+    def __init__(self, fn):
+        ClickReview.__init__(self, fn, "bin-path")
+
+        self.bin_paths = dict()
+        for app in self.manifest['hooks']:
+            self.bin_paths[app] = self._extract_bin_path(app)
+
+    def _extract_bin_path(self, app):
+        '''Get bin-path for app'''
+        rel = self.manifest['hooks'][app]['bin-path']
+        fn = os.path.join(self.unpack_dir, rel)
+        if not os.path.exists(fn):
+            error("Could not find '%s'" % rel)
+        return fn
+
+    def _check_bin_path_executable(self, app):
+        '''Check that the provided path exists'''
+        rel = self.manifest['hooks'][app]['bin-path']
+        fn = os.path.join(self.unpack_dir, rel)
+        return os.access(fn, os.X_OK)
+
+    def check_path(self):
+        '''Check path exists'''
+        t = 'info'
+        n = 'path exists'
+        s = "OK"
+
+        for app in sorted(self.bin_paths):
+            t = 'info'
+            n = 'path executable'
+            s = "OK"
+            if not self._check_bin_path_executable(app):
+                t = 'error'
+                s = "'%s' is not executable" % \
+                    (self.manifest['hooks'][app]['bin-path'])
+            self._add_result(t, n, s)
---
+++ clickreviews/tests/test_cr_bin_path.py
@@ -0,0 +1,47 @@
+'''test_cr_bin_path.py: tests for the cr_bin_path module'''
+#
+# Copyright (C) 2014 Canonical Ltd.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from clickreviews.cr_bin_path import ClickReviewBinPath
+import clickreviews.cr_tests as cr_tests
+
+
+class TestClickReviewBinPath(cr_tests.TestClickReview):
+    """Tests for the lint review tool."""
+    def setUp(self):
+        # Monkey patch various file access classes. stop() is handled with
+        # addCleanup in super()
+        cr_tests.mock_patch()
+        super()
+
+    def test_check_path(self):
+        '''Test check_path()'''
+        self.set_test_bin_path(self.default_appname, "bin/foo.exe")
+        c = ClickReviewBinPath(self.test_name)
+        c.check_path()
+        r = c.click_report
+        # We should end up with 1 info
+        expected_counts = {'info': 1, 'warn': 0, 'error': 0}
+        self.check_results(r, expected_counts)
+
+    def test_check_path_nonexistent(self):
+        '''Test check_path() - nonexecutable'''
+        self.set_test_bin_path(self.default_appname, "bin/foo.nonexec")
+        c = ClickReviewBinPath(self.test_name)
+        c.check_path()
+        r = c.click_report
+        # We should end up with 1 info
+        expected_counts = {'info': 0, 'warn': 0, 'error': 1}
+        self.check_results(r, expected_counts)
