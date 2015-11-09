612c8b70aa3c77d659bc0c755ba7e376556ba1ff
Daniel Holbach <daniel.holbach@canonical.com>
2015-11-09 09:53:24 +0100

    pass absolute path of click or snap file - that way it's safe even if we chdir

--- clickreviews/cr_common.py
+++ clickreviews/cr_common.py
@@ -663,12 +663,14 @@ def _unpack_cmd(cmd_args, d, dest):
 def _unpack_snap_squashfs(snap_pkg, dest):
     '''Unpack a squashfs based snap package to dest'''
     d = tempfile.mkdtemp(prefix='clickreview-')
-    return _unpack_cmd(['unsquashfs', '-f', '-d', d, snap_pkg], d, dest)
+    return _unpack_cmd(['unsquashfs', '-f', '-d', d, 
+                        os.path.abspath(snap_pkg)], d, dest)
 
 
 def _unpack_click_deb(click_pkg, dest):
     d = tempfile.mkdtemp(prefix='clickreview-')
-    return _unpack_cmd(['dpkg-deb', '-R', click_pkg, d], d, dest)
+    return _unpack_cmd(['dpkg-deb', '-R', 
+                        os.path.abspath(click_pkg), d], d, dest)
 
 
 def unpack_click(fn, dest=None):
