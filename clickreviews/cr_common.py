472234208c6ce07bb6865d93551b4f3e351ef19b
Jamie Strandboge <jamie@ubuntu.com>
2015-12-02 13:16:38 -0600

    add 'dtbs' snappy key for armhf kernel snaps

--- clickreviews/cr_common.py
+++ clickreviews/cr_common.py
@@ -96,6 +96,7 @@ class ClickReview(object):
                        "binaries",
                        "caps",
                        "config",
+                       "dtbs",
                        "firmware",
                        "frameworks",
                        "icon",
