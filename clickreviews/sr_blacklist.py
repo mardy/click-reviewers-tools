04160c2afcb2bcc21123a7ea4b078ba22cf626bb
Matt Goodall <matt.goodall@canonical.com>
2016-04-28 17:23:57 +0100

    Use single quotes and fix docstring.

--- clickreviews/sr_blacklist.py
+++ clickreviews/sr_blacklist.py
@@ -3,17 +3,17 @@ from clickreviews.sr_common import SnapReview
 # TODO(matt): where shall I load this list from? is it going to take a long
 # time and use a lot of memory?
 blacklist = set([
-    "blacklisted-name",
+    'blacklisted-name',
 ])
 
 class SnapReviewBlacklist(SnapReview):
 
     def __init__(self, fn, overrides=None):
         # TODO(matt): is this a good name?
-        SnapReview.__init__(self, fn, "blacklist-snap", overrides=overrides)
+        SnapReview.__init__(self, fn, 'blacklist-snap', overrides=overrides)
 
     def check_package_name(self):
-        """Trigger a manual review if the package name is blacklisted."""
+        '''Trigger a manual review if the package name is blacklisted'''
         # TODO(matt): is this correct?
         if not self.is_snap2:
             return
