39e7806270bae351bd8efea262f430b5829b5a55
Jamie Strandboge <jamie@ubuntu.com>
2016-06-30 14:24:07 -0500

    clickreviews/sr_security.py: whitespace for previous commit

--- clickreviews/sr_security.py
+++ clickreviews/sr_security.py
@@ -196,8 +196,7 @@ class SnapReviewSecurity(SnapReview):
             t = 'info'
             n = self._get_check_name('profile_name_length', app=app)
             s = "OK"
-            profile = "snap.%s.%s" % (self.snap_yaml['name'],
-                                    app)
+            profile = "snap.%s.%s" % (self.snap_yaml['name'], app)
             if len(profile) > maxlen:
                 t = 'error'
                 s = ("'%s' too long (exceeds %d characters). Please shorten "
