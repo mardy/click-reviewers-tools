bb5fa49a2ad952914f1b9d8891a991c0f885efbc
Jamie Strandboge <jamie@ubuntu.com>
2017-06-23 11:37:34 -0500

    rename as bin/create-snap-declaration update setup.py to install bin/create-snap-declaration

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ if os.path.exists(changelog):
         version = match.group(1)
 
 scripts = glob.glob('bin/click-*') + glob.glob('bin/snap-*')
+scripts.append('bin/create-snap-declaration')
 scripts.remove('bin/click-check-skeleton')
 scripts.remove('bin/snap-check-skeleton')
 setup(
